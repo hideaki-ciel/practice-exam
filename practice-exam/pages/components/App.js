4d74ac0676f2c8fcd6ea205a4a8da76782da7e34
hideaki-ciel <h-inayoshi@ciel-japan.jp>
2021-06-04 17:42:08 +0900

    App.jsとheader.js追加

---
+++ practice-exam/pages/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './header'
+//import {main} from './main'
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                <Header />
+                {/* <Main /> */}
+                {/* <footer /> */}
+            </div>
+        )
+    };
+}
+export default App;
---
+++ practice-exam/pages/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+ 
+class Header extends React.Component{ 
+    render() {
+        return (
+            <header>
+                <meta charSet="utf-8"></meta>
+                <title>practice-exam</title>
+                <h1>実教Web模擬試験システム</h1>       
+            </header>
+        )
+    };
+};
+export default Header
